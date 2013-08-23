daf2fe699ad1f3ac4e0fde42fdbcf55581bb663a
yunlongxiao <yunlongxiao@anjuke.com>
2013-08-23 11:05:09 +0800

    cpu_mem 1.0

--- app/views/admin/cpu_mem.py
+++ app/views/admin/cpu_mem.py
@@ -1,10 +1,55 @@
 # -*- coding: utf-8 -*-
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, jsonify
+from wtforms import Form, BooleanField, TextField, validators
+from operator import itemgetter, attrgetter
 
 mod = Blueprint('cpu_mem', __name__, url_prefix='/admin/cpu_mem')
 
+from app import app, db
+from app.models import *
 
-@mod.route('/')
+
+@mod.route('/', methods=['GET'])
 def index():
-    return render_template('admin/cpu_mem/index.html')
+        rows = CPU_Mem.query.all()
+        return render_template('admin/cpu_mem/index.html', rows=rows)
+
+
+@mod.route('/add/', methods=['POST'])
+def add():
+    cpu = request.form["cpu"]
+    mem = request.form["mem"]
+    if not cpu.isdigit():
+        return jsonify(msg="cpu need be digit")
+    if not mem.isdigit():
+        return jsonify(msg="mem need be digit")
+    c_m = CPU_Mem(cpu, mem)
+    db.session.add(c_m)
+    db.session.commit()
+    return jsonify(id=c_m.id)
+
+
+@mod.route('/update/', methods=['POST'])
+def update():
+    id = request.form["id"]
+    cpu = request.form["cpu"]
+    mem = request.form["mem"]
+    if not cpu.isdigit():
+        return jsonify(msg="cpu need be digit")
+    if not mem.isdigit():
+        return jsonify(msg="mem need be digit")
+    c_m = CPU_Mem.query.get(id)
+    c_m.cpu = cpu
+    c_m.mem = mem
+    db.session.commit()
+    return jsonify(flag=1)
+
+
+@mod.route('/delete/', methods=['POST'])
+def delete():
+    id = request.form["id"]
+    c_m = CPU_Mem.query.get(id)
+    db.session.delete(c_m)
+    db.session.commit()
+    return jsonify(flag=1)
